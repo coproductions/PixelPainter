d2625d87c99ac70d04f83deb4d2e0c90477aec70
Constanti <constantin.dumba@gmail.com>
2015-07-25 19:01:10 -1000

    compression for url save implemented

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -5,6 +5,50 @@ window.onload = function(){
   if(window.location.hash){
     pixelPaintRunner.renderHash();
   }
+
+  $("#full").spectrum({
+    color: "#ECC",
+    showInput: true,
+    className: "full-spectrum",
+    showInitial: true,
+    showPalette: true,
+    showSelectionPalette: true,
+    maxSelectionSize: 10,
+    preferredFormat: "hex",
+    localStorageKey: "spectrum.demo",
+    move: function (color) {
+
+    },
+    show: function () {
+
+    },
+    beforeShow: function () {
+
+    },
+    hide: function () {
+
+    },
+    change: function(color) {
+      console.log(color.toString())
+      pixelPaintRunner.pickColorFromPicker(color);
+    },
+    palette: [
+        ["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)",
+        "rgb(204, 204, 204)", "rgb(217, 217, 217)","rgb(255, 255, 255)"],
+        ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
+        "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"],
+        ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)",
+        "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)",
+        "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)",
+        "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)",
+        "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)",
+        "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
+        "rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
+        "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
+        "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)",
+        "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
+    ]
+  });
 };
 
 
@@ -63,52 +107,92 @@ var pixelPainter = function(){
 
   window.onhashchange = renderHash;
 
+
   function renderHash(){
     var hash = window.location.hash.substr(1);
     if(hash){
-      var savedImageDataObj = deSerialize(hash);
-      resolutionInputRender(null,savedImageDataObj.gridSize);
+      var dataObj = deSerialize(hash);
+      resolutionInputRender(null,dataObj.gridSize);
 
       var ppCells = document.querySelectorAll('.ppCell');
-        for (var i = 0; i < ppCells.length; i++) {
-          if(i.toString() in savedImageDataObj){
-            ppCells[i].style.backgroundColor = '#'+ savedImageDataObj[i.toString()];
-          }
-        };
+
+      for (var i = 0; i < ppCells.length; i++) {
+        if(i.toString() in dataObj.pixels){
+          ppCells[i].style.backgroundColor = '#'+ dataObj.colors[dataObj.pixels[i.toString()]];
+        }
+      };
     }
   };
 
   function saveImageToUrl(){
-    var savedImageData = {};
+    var savedCellValues = {};
+    var savedColors = {};
+    var id = 1;
+    var referenceId;
     var ppCells = document.querySelectorAll('.ppCell');
+
     var gridSize = Math.sqrt(ppCells.length);
     for (var i = 0; i < ppCells.length; i++) {
       if(ppCells[i].style.backgroundColor && ppCells[i].style.backgroundColor !== 'white'){
-        savedImageData[i] = rgbToHex(ppCells[i].style.backgroundColor);
+        if(!(rgbToHex(ppCells[i].style.backgroundColor) in savedColors)){
+          savedColors[rgbToHex(ppCells[i].style.backgroundColor)] = id;
+          referenceId = id;
+          id++;
+        }else{
+          referenceId = savedColors[rgbToHex(ppCells[i].style.backgroundColor)]
+        }
+        savedCellValues[i] = referenceId;
       }
     };
     // console.log(serializeObject(savedImageData,gridSize));
-    window.location.href = '#' + serializeObject(savedImageData,gridSize);
+    window.location.href = '#' + serializeObject(savedColors,savedCellValues,gridSize);
     savePrompt.innerHTML = 'please bookmark this page to save your image';
   }
 
-  function serializeObject(obj,gridSize){
+  function serializeObject(colors,cellValues,gridSize){
     var resultStr = gridSize;
-    for(var key in obj){
-      resultStr += '&' + key+obj[key];
+    for(var key in colors){
+      resultStr += '&' + key+colors[key];
     }
+    resultStr += 'z';
+    for(var key in cellValues){
+      resultStr += '&' + key + 'i' + cellValues[key];
+    }
+
     return resultStr;
   };
 
+
   function deSerialize(str){
-    var strArray = str.split('&');
-    var deSerializedObj = {};
-    deSerializedObj.gridSize = strArray.shift();
-    for (var i = 0; i < strArray.length; i++) {
-      deSerializedObj[strArray[i].slice(0,strArray[i].length-6)]
-      = strArray[i].slice(strArray[i].length-6,strArray[i].length);
+    var deSerialized = {};
+    var duplex = str.split('z');
+    var colorArray = duplex[0].split('&');
+    var cellArray = duplex[1].split('&');
+
+    var colors = {};
+    var pixels = {};
+
+    //extracting the gridsize value
+    deSerialized.gridSize = colorArray.shift();
+
+    //generating color object
+    for (var i = 0; i < colorArray.length; i++) {
+      colors[colorArray[i].slice(6,colorArray[i].length)]
+      = colorArray[i].slice(0,6);
     };
-    return deSerializedObj;
+
+    //generating cell/pixel info object
+
+    for (var i = 0; i < cellArray.length; i++) {
+      var cellAndId = cellArray[i].split('i');
+      pixels[cellAndId[0]] = cellAndId[1];
+    };
+
+    //putting object together
+    deSerialized.colors = colors;
+    deSerialized.pixels = pixels;
+
+    return deSerialized;
   }
 
   function rgbToHex(str){
@@ -162,17 +246,19 @@ var pixelPainter = function(){
 
 
   function clearCanvas(){
-     if(selectedColorElement){
+    if(selectedColorElement){
       selectedColorElement.className = 'ppSwatchCell';
     }
-    Array.prototype.forEach.call(ppGridCells,function(val){
-    val.style.backgroundColor = 'white';
-    undoArray = [];
-    ndoHistoryArray = [];
-    selectedColor = null;
-     });
+    // Array.prototype.forEach.call(ppGridCells,function(val){
+    //   val.style.backgroundColor = 'white';
+    //   undoArray = [];
+    //   ndoHistoryArray = [];
+    //   selectedColor = null;
+    //  });
     window.location.hash = '';
     savePrompt.innerHTML = '';
+    resolutionInputRender(null,15);
+    resolutionInput.value = '';
   };
 
   function erase(){
@@ -211,6 +297,13 @@ var pixelPainter = function(){
     }
     this.style.backgroundColor = selectedColor;
   };
+
+  function pickColorFromPicker(color){
+    selectedColor = color;
+    if(selectedColorElement){
+      selectedColorElement.className = 'ppSwatchCell';
+    }
+  }
 
   var pickColor = function(){
     if(selectedColorElement){
@@ -269,11 +362,16 @@ var pixelPainter = function(){
       val.style.width = cellSizeExBorder+'px';
       val.style.height = cellSizeExBorder+'px';
       });
+    resolutionInput.value = '';
 
   }
 
+
+
+
   // generates the pp swatch
   var swatchGenerator = function(colorArray){
+    console.log('colorArray',colorArray)
     var ppSwatch = document.createElement('div');
     ppSwatch.id = 'ppSwatch'
     var ppSwatchArea = document.createElement('div');
@@ -292,6 +390,16 @@ var pixelPainter = function(){
     logo.appendChild(logoText);
     ppSwatchArea.appendChild(logo);
     ppSwatchArea.appendChild(clearButton);
+
+    var colorPicker = document.createElement('input');
+    colorPicker.id = 'full';
+    colorPicker.type = 'text';
+
+    $("#custom").spectrum({
+        flat: true,
+        showInput: true,
+        allowEmpty:true
+    });
 
     //generating swatch rows
     while(nrOfRows > 0){
@@ -313,6 +421,7 @@ var pixelPainter = function(){
       nrOfRows --;
     }
 
+    ppSwatchArea.appendChild(colorPicker);
     ppSwatchArea.appendChild(ppSwatch);
     ppSwatchArea.appendChild(eraseButton);
     ppSwatchArea.appendChild(undoButton);
@@ -394,6 +503,7 @@ var pixelPainter = function(){
     buildSwatch : swatchGenerator,
     buildFromObject : buildFromObject,
     randomColorGenerator : randomColorGenerator,
-    renderHash: renderHash
+    renderHash: renderHash,
+    pickColorFromPicker: pickColorFromPicker
   };
 };
